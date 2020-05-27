8114c30863df802cde8ad55e5c537e54556ac8d6
Zachary Richards <zachprichards@gmail.com>
2020-05-27 11:34:59 -0700

    removed previous attempts and starting over

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ var ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var mouse = {
-    x: 0,
-    y: 0,
-    clicked: false,
-    clickDown: false
-}
+// var mouse = {
+//     x: 0,
+//     y: 0,
+//     clicked: false,
+//     clickDown: false
+// }
 
-//Events
+//Events Listeners
 window.addEventListener('mousemove', 
     function(event){
     mouse.x = event.x;
@@ -20,141 +20,52 @@ window.addEventListener('mousemove',
     console.log("x:" + mouse.x + ", y:" + mouse.y);
 });
 
+document.addEventListener("DOMContentLoaded", function(){
+    start();
+});
 
 
 var fishes = [];
 var debug = false;
 
+function  start(){
+    //Instantiating a fish object in the center of the page for testing purposes
+    fishes.push(new Fish(canvas.width / 2, canvas.height / 2));
+
+    draw();
+}
+
 function Fish(x, y)
 {
     this.x = x;
     this.y = y;
-    this.rotation = 0;
-    this.turningSpeed = .1;
-    this.speed = 1;
 
-    this.draw = function(t) {
-
-        var scale = 40;
-        
-        var tailTime = (t - 3);
-        var tailHandleTime = t - 4;
-
-        //Tail horizontal & vertical travel
-        var tht = .25;
-        var tvt = 1;
-
-        var tailOffsetX = 120;
-
-        var tx = tailOffsetX + scale * -tht * Math.cos(2 * tailTime) + tht;
-        var ty = scale * -tvt * Math.cos(tailTime) + tvt;
-
-        //Tail handle horizional & vertical travel
-        var thht = .2;
-        var thvt = 1.5;
-
-        var tailHOffsetX = 90;
-
-        var thx = tailHOffsetX + scale * thht * Math.cos(2 * tailHandleTime) + thht;
-        var thy = scale * thvt * Math.cos(tailHandleTime) + thvt;
-
-        //Head handle horizional & vertical travel
-        var hhht = .5;
-        var hhvt = 1;
-
-        var headOffsetX = 40;
-        var headHandleLength = 20;
-        
-        var hhx = headOffsetX + headHandleLength * -hhht * Math.cos(2 * t) + hhht;
-        var hhy = headHandleLength * -hhvt * Math.cos(t) + hhvt;
-
-        var headThickness = 20;
-        var thickness = 20;
-
-        //this.rotation -= (hhy / 30);
+    this.draw = function() {
 
         ctx.save();
         ctx.translate(this.x, this.y);
-    
-
-        ctx.globalAlpha = 1;
-
-        ctx.rotate(this.rotation);
-        ctx.beginPath();
-        ctx.moveTo(0, 0);
-        ctx.bezierCurveTo(hhx - headThickness, hhy - headThickness, thx, thy, tx, ty);
-        ctx.stroke();
+        ctx.globalAlpha = 1;   
         
-        ctx.beginPath();
-        ctx.moveTo(0, 0);
-        ctx.bezierCurveTo(hhx - headThickness, hhy + headThickness, thx, thy + thickness, tx, ty);
-        ctx.stroke();
+        //Draw -----
 
-        ctx.globalAlpha = 1;
 
-        if(debug)
-        {
-            ctx.beginPath();
-            ctx.moveTo(tx, ty);
-            ctx.lineTo(thx, thy);
-            ctx.stroke();
-
-            ctx.beginPath();
-            ctx.moveTo(0, 0);
-            ctx.lineTo(hhx, hhy);
-            ctx.stroke();
-        }
-    
         ctx.restore();
     }
     
-    this.update = function(time){
-        var mouseRad = (Math.atan2((mouse.y - this.y), (mouse.x - this.x)) + Math.PI)
-        this.rotation += shortAngleDist(this.rotation, mouseRad) * this.turningSpeed;
-        
-        this.x -= Math.cos(this.rotation) * this.speed;
-        this.y -= Math.sin(this.rotation) * this.speed;
-
-        this.speed = (animationSpeed + 1);
-
-        this.draw(time);
+    this.update = function() {
+        this.draw();
     }
 }
 
-var t = 0;
-
-fishes.push(new Fish(200, 200));
-
-
-var speedChangeInterval = 150;
-var speedChangeTimer = 0;
-
-var animationSpeed = .5;
-var targetAnimationSpeed = .5;
-var animStartTime = 0;
-
-
 function draw() {
-
-
-    if(speedChangeTimer < speedChangeInterval)
-        speedChangeTimer++;
-    else
-    {
-        speedChangeTimer = 0;
-        targetAnimationSpeed = Math.random();
-        animStartTime = t;
-    }
-
-
-    animationSpeed = lerp(animationSpeed, targetAnimationSpeed, (t - animStartTime) / speedChangeInterval);
     requestAnimationFrame(draw);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    t += animationSpeed + 1;
 
-    var time = t / 20;
-    fishes[0].update(time);
+    for(var i = 0; i < fishes.length; i++)
+    {
+        fishes[i].update();
+    }
 }
 
 function shortAngleDist(a0,a1) {
@@ -163,12 +74,6 @@ function shortAngleDist(a0,a1) {
     return 2*da % max - da;
 }
 
-draw();
-
-$(document).keypress(function(event){
-    draw();
+$( document ).ready(function() {
+    console.log( "ready!" );
 });
-
-function lerp(start, end, t) {
-    return start * (1 - t) + end * t
-}
